6a36f6e03b66a19b91cda007f0aadf6212a694fd
Jesus Rodriguez <jesrodmiranda@gmail.com>
2017-07-10 17:02:30 -0500

    Ajustes HTML de un nuevo mockup - parte 1

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -2,21 +2,21 @@ $(function(){
     $(window).bind("load resize", function(){
         _winWidth = $(window).width();
         _winHeight= $(window).height();
-
-        // Setting Width
-        $('.side-1').css({'width':_winWidth * 0.15}); // 0.15 = 15%
-        console.log('Width: ' + _winWidth);
-        $('.side-2').css({'width':_winWidth * 0.15}); // 0.15 = 15%
         
         _wTop = 5; 
         
-        if ( _winWidth > 1700) {
+        if ( _winWidth > 1800) {
             _wTop = 5;
+            $('.logo object').css({'width':270, 'margin-top': _wTop}); // 0.15 = 15%
+        } else if ( _winWidth > 1300) {
+            _wTop = 10;
+            $('.logo object').css({'width':230, 'margin-top': _wTop}); // 0.15 = 15%
         } else if (_winWidth > 1000 ) {
             _wTop = 13;
+            $('.logo object').css({'width':_winWidth * 0.20, 'margin-top': _wTop}); // 0.15 = 15%
         }
         
-        $('.logo object').css({'width':_winWidth * 0.15, 'margin-top': _wTop}); // 0.15 = 15%
+        
         
     });
 });
